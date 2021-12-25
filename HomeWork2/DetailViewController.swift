6859464e27f00b5ae4fc27d78b02c8e6eb6b7e35
Sinan Kulen <sinan.kulen@gmail.com>
2021-12-25 16:04:34 +0300

    added notification center

--- HomeWork2/DetailViewController.swift
+++ HomeWork2/DetailViewController.swift
@@ -9,22 +9,30 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var detailLabel: UILabel!
     var emergencyDelegate : DataTransferProtocol?
     var dataTransferClosure : ((String) -> Void)?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NotificationCenter.default.addObserver(self, selector: #selector(health), name: Notification.Name("postName"), object: nil)
     }
-
+    //MARK: NotificationCenter ile gelen veriyi detailLAbel ile ekrana yazdırıyoruz.
+    @objc func health(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        if let info = userInfo["doctor"] as? String {
+            self.detailLabel.text = "\(info)"
+        }
+    }
  
     @IBAction func emergencyButton(_ sender: UIButton) {
-         //Delegate ile butona basıldığında phoneNumber'ı ViewController'a iletmiş oluyoruz.
-        // dismiss(animated: true) {
-       //  self.emergencyDelegate?.emergencyCall(phoneNumber: (sender.titleLabel?.text)!)
-      //  }
+         // MARK: Delegate ile butona basıldığında phoneNumber'ı ViewController'a iletmiş oluyoruz.
+        dismiss(animated: true) {
+            self.emergencyDelegate?.emergencyCall(phoneNumber: (sender.titleLabel?.text)!)
+        }
         
-        //Closure ile butona basıldığında button title'ı ViewController'a iletmiş oluyoruz.
+        //MARK: Closure ile butona basıldığında button title'ı ViewController'a iletmiş oluyoruz.
         dismiss(animated: true) { [weak self] in
             self?.dataTransferClosure?((sender.titleLabel?.text!)!)
         }
--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -13,27 +13,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   
     }
 
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-        // Delegate ile gelen veri yazdırıldı.
+        //MARK: Delegate ile gelen veri yazdırıldı.
         if segue.identifier == "delegateSegue" {
             if let detailVC = segue.destination as? DetailViewController {
                 detailVC.emergencyDelegate = self
             }
             
-        // Closure ile gelen veri transferLabel ile ekrana yazdırıldı.
+        //MARK: Closure ile gelen veri transferLabel ile ekrana yazdırıldı.
         } else if segue.identifier == "closureSegue" {
             if let detailVC = segue.destination as? DetailViewController {
                 detailVC.dataTransferClosure = { [weak self] phoneNumber in
                     self!.transferLabel.text = phoneNumber
                 }
             }
+        //MARK: Notification Center ile veriyi alması için DetailViewController'a gönderildi.
+        } else if segue.identifier == "notificationSegue" {
+            if segue.destination is DetailViewController {
+            let response = ["doctor":"patient"]
+            NotificationCenter.default.post(name: Notification.Name("postName"), object: nil, userInfo: response)
+            }
         }
     }
-
+    
 }
 
 extension ViewController: DataTransferProtocol {
